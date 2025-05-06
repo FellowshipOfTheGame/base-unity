b6216abb34f9929383ad3a08f0b6857b79a2e6c1
Gabriel Barbosa de Oliveira <gabrieloliveira00@outlook.com>
2025-05-06 18:20:45 -0300

    updated base project

---
+++ Assets/Scripts/Jogador/Movimento.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    [SerializeField] private float Velocidade;
+    [SerializeField] private Transform PeDoPersonagem;
+    [SerializeField] private LayerMask Chao;
+    
+    
+    //O corpo do jogador
+    [SerializeField] private Rigidbody2D Corpo;
+    //Para ele não pular infinitamente
+    private bool PodePular = false;
+    [SerializeField] private float ForcaPulo;
+
+    void Update()
+    {
+        //Define a velocidade do corpo baseada na tecla pressionada (Input.GetAxisRaw("Horizontal"))
+        //A função retorna 1 se a seta pra direita ou D foram pressionados
+        //Retorna -1 se a seta da esquerda ou A foram pressionados
+        //Retorna 0 se nenhum direcional foi pressionado
+        float movimento_horizontal = Velocidade * Input.GetAxisRaw("Horizontal");
+
+        //Neste caso, não se usa Time.deltaTime, porque RigidBody2D.velocity já opera baseado na taxa de frames
+        Corpo.linearVelocity = new Vector2(movimento_horizontal, Corpo.linearVelocity.y);
+
+        //Cria uma caixa, se a caixa colidir com o chao, pode pular
+        //Nessa função se passa a posição, tamanho, angulo e distancia(tamanho) em relação a direção
+        //Tambem passa um layer mask, pra que somente os layers associados a Chao sejam considerados
+        bool PertoDoChao = Physics2D.BoxCast(PeDoPersonagem.position, new Vector2(0.5f, 0.2f), 0f, Vector2.down, 0.1f, Chao);
+                
+        //Se o acerto tem um resultado não nulo, pode pular
+        if(PertoDoChao)
+        {
+            PodePular = true;
+        }
+        else //Caso contrário, não se pode pular
+        {
+            PodePular = false;
+        }
+
+        //Se a barra de espaço foi pressionada e o jogador pode pular
+        if (Input.GetKeyDown(KeyCode.Space) && PodePular)
+        {
+            //Adiciona uma força para cima proporcional à ForçaPulo
+            Corpo.AddForce(Vector2.up * ForcaPulo);
+            //Proíbe o jogador de pular
+            PodePular = false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# base-unity
+Versão da Unity: 6000.1.1f1
